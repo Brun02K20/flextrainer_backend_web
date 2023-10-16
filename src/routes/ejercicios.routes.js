95ce7387002514710e9fed38ab456954c9a173f5
Brun02K23 <bvirinni@gmail.com>
2023-10-16 02:25:25 -0300

    solucionando el tema de los videos

--- src/routes/ejercicios.routes.js
+++ src/routes/ejercicios.routes.js
@@ -2,6 +2,7 @@ import express from "express"; // importo la libreria express
 import { ejerciciosServices } from "../services/ejercicios.service.js"; // importo los servicios necesarios
 const router = express.Router(); // creo el objeto de routeo
 
+// endpoint para obtener un ejercicio segun su id
 router.get('/ejercicio/:id', async (req, res, next) => {
     try {
         const rdo = await ejerciciosServices.getEjercicioById(req.params.id);
--- src/services/ejercicios.service.js
+++ src/services/ejercicios.service.js
@@ -1,6 +1,7 @@
 import sequelize from "../databases/databases.js";
 import { Op } from "sequelize";
 
+// obtener un ejercicio segun su id, para mostrar sus datos
 const getEjercicioById = async (id) => {
     const rdo = await sequelize.models.Ejercicios.findByPk(id, {
         include: [
@@ -24,6 +25,7 @@ const getEjercicioById = async (id) => {
     } else {
         rdo.dataValues.nombreZonaCuerpo = rdo.dataValues.Cuerpo_Zona.nombre;
         rdo.dataValues.nombreCategoria = rdo.dataValues.Categoria_Ejercicio.nombre;
+        rdo.dataValues.linkVideo = rdo.dataValues.Video ? rdo.dataValues.Video.nombre : ''
         delete rdo.dataValues.Cuerpo_Zona;
         delete rdo.dataValues.Categoria_Ejercicio;
         return rdo.dataValues
